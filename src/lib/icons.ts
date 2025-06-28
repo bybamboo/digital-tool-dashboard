10286955711f7c71dd55a9602025b34282abb6f7
Bamboo <hola@sandrabamboo.com>
2025-06-28 09:41:36 +0200

    Update icons.ts

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,7 +1,6 @@
 import * as Icons from 'lucide-react';
 
-// Recibe el nombre como string y devuelve el componente correspondiente
 export const getLucideIcon = (iconName: string) => {
-  const LucideIcon = Icons[iconName as keyof typeof Icons];
-  return LucideIcon || Icons.FolderOpen;
+  if (!iconName) return Icons.FolderOpen;
+  return Icons[iconName as keyof typeof Icons] || Icons.FolderOpen;
 };
